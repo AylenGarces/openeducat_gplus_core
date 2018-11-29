a84a696e503facd5a1290664b6a46bfde51b24ea
manuel <manuel.bouza07@gmail.com>
2018-11-29 13:06:14 -0500

    probando GitHub con una asignatura

---
+++ models/subject.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+
+class OpSubject(models.Model):
+    _name = 'op.subject'
+
+    name = fields.Char('Name', size=128, required=True)
+    code = fields.Char('Code', size=256, required=True)
+    course_id = fields.Many2one('op.course', 'Course')
+    grade_weightage = fields.Float('Grade Weightage')
+    type = fields.Selection(
+        [('theory', 'Theory'), ('practical', 'Practical'),
+         ('both', 'Both'), ('other', 'Other')],
+        'Type', default="theory", required=True)
