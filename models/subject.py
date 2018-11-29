fa515807755e37d2697d70cf90670c76f0710f1f
manuel <manuel.bouza07@gmail.com>
2018-11-29 13:31:45 -0500

    Estado Original

--- models/subject.py
+++ models/subject.py
@@ -6,8 +6,8 @@ from openerp import models, fields
 class OpSubject(models.Model):
     _name = 'op.subject'
 
-    name = fields.Char('Name1', size=128, required=True)
-    code = fields.Char('Code2', size=256, required=True)
+    name = fields.Char('Name', size=128, required=True)
+    code = fields.Char('Code', size=256, required=True)
     course_id = fields.Many2one('op.course', 'Course')
     grade_weightage = fields.Float('Grade Weightage')
     type = fields.Selection(
