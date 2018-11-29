12f58da04ec50f9e36feb9d26cb3a9fcfbd97545
manuel <manuel.bouza07@gmail.com>
2018-11-29 13:12:16 -0500

    probando GitHub con una asignatura, cambio menor

--- models/subject.py
+++ models/subject.py
@@ -6,7 +6,7 @@ from openerp import models, fields
 class OpSubject(models.Model):
     _name = 'op.subject'
 
-    name = fields.Char('Name', size=128, required=True)
+    name = fields.Char('Name1', size=128, required=True)
     code = fields.Char('Code', size=256, required=True)
     course_id = fields.Many2one('op.course', 'Course')
     grade_weightage = fields.Float('Grade Weightage')
