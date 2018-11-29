cfb22623c40b10063e6ba2ac3320bb64628bc4b4
manuel <manuel.bouza07@gmail.com>
2018-11-29 13:16:13 -0500

    probando GitHub con una asignatura, cambio menor2

--- models/subject.py
+++ models/subject.py
@@ -7,7 +7,7 @@ class OpSubject(models.Model):
     _name = 'op.subject'
 
     name = fields.Char('Name1', size=128, required=True)
-    code = fields.Char('Code', size=256, required=True)
+    code = fields.Char('Code2', size=256, required=True)
     course_id = fields.Many2one('op.course', 'Course')
     grade_weightage = fields.Float('Grade Weightage')
     type = fields.Selection(
